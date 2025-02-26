c4b294fc8aa635731066025303b075a311d9a6b5
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-26 19:59:31 +0200

    wrap GenerateButton with provider and call function generateImage

--- lib/presentation/widgets/generate_button.dart
+++ lib/presentation/widgets/generate_button.dart
@@ -1,28 +1,34 @@
+import 'package:ai_image_generator/presentation/provider/image_generator_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 class GenerateButton extends StatelessWidget {
   const GenerateButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: ElevatedButton.icon(
-        onPressed: () {},
-        icon: const Icon(Icons.auto_awesome, color: Colors.white),
-        label: Padding(
-          padding: EdgeInsets.symmetric(vertical: 14.h),
-          child: Text("Generate Image",
-              style: TextStyle(fontSize: 18.sp, color: Colors.white)),
-        ),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.deepPurpleAccent,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.r),
+    return Consumer<ImageGeneratorProvider>(
+      builder: (context, provider, child) {
+        return SizedBox(
+          width: double.infinity,
+          child: ElevatedButton.icon(
+            onPressed: () => provider.generateImage(),
+            icon: const Icon(Icons.auto_awesome, color: Colors.white),
+            label: Padding(
+              padding: EdgeInsets.symmetric(vertical: 14.h),
+              child: Text("Generate Image",
+                  style: TextStyle(fontSize: 18.sp, color: Colors.white)),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurpleAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.r),
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
