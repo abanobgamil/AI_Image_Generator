2f716815fb2fb349530e140670c31e5b685df109
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-26 17:58:22 +0200

    Integrate `ImageGeneratorProvider` and `ImageSection` into `AiTextToImageGenerator` screen, wrapping it with a `Provider` and adding `ImageSection` widget.

--- lib/presentation/screen/ai_text_to_image_generator.dart
+++ lib/presentation/screen/ai_text_to_image_generator.dart
@@ -1,43 +1,53 @@
+import 'package:ai_image_generator/presentation/provider/image_generator_provider.dart';
 import 'package:ai_image_generator/presentation/widgets/generate_button.dart';
+import 'package:ai_image_generator/presentation/widgets/image_section.dart';
 import 'package:ai_image_generator/presentation/widgets/input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 class AiTextToImageGenerator extends StatelessWidget {
   const AiTextToImageGenerator({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.deepPurple.shade300, Colors.indigo.shade700],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+    return Provider<ImageGeneratorProvider>(
+      create: (BuildContext context) {
+        return  ImageGeneratorProvider();
+      },
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple.shade300, Colors.indigo.shade700],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-        ),
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.w,vertical: 50.h),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                FittedBox(
-                  child: Text(
-                    "AI Image Generator",
-                    style: TextStyle(
-                      fontSize: 36.sp,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w,vertical: 50.h),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  FittedBox(
+                    child: Text(
+                      "AI Image Generator",
+                      style: TextStyle(
+                        fontSize: 36.sp,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 30.h),
-                 const InputField(),
-                SizedBox(height: 20.h),
-                const GenerateButton(),
-              ],
+                  SizedBox(height: 30.h),
+                   const InputField(),
+                  SizedBox(height: 20.h),
+                  const GenerateButton(),
+                  SizedBox(height: 30.h),
+                  const ImageSection(),
+                ],
+              ),
             ),
           ),
         ),
